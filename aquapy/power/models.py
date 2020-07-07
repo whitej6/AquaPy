258afaf8369b09c799ddf52647df34b2f493d322
Jeremy White <jeremy.white@networktocode.com>
2020-07-07 08:20:08 -0500

    adding changes to model

--- aquapy/power/models.py
+++ aquapy/power/models.py
@@ -5,9 +5,16 @@ class WemoSwitch(models.Model):
     """
 
     """
+    DESIRED_STATE_CHOICES = [
+        'On',
+        'Off',
+        'Scheduled'
+    ]
+
     name = models.CharField(max_length=255)
     mac_address = models.CharField(max_length=255, unique=True)
     device_type = models.CharField(max_length=255)
+    desired_state = models.CharField(max_length=255, choices=DESIRED_STATE_CHOICES)
 
     def __str__(self):
         return self.name
